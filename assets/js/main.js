ad2fafbcf04c03fa06494d0d114d55da2d071dc5
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-09 19:17:26 -0500

    day counter for christmas

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,4 +28,74 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== DAY COUNTER FOR CHRISTMAS ===============*/
+const titleData = document.getElementById('title-data'),
+    numberData = document.getElementById('number-data'),
+    textData = document.getElementById('text-data'),
+    msgChristmas = document.getElementById('msg-christmas')
 
+const christmasCountdown = () => {
+    let now = new Date(), // Get today's date
+        currentMonth = now.getMonth() + 1, // Get the current month
+        currentDay = now.getDate() // Get the current day of the month
+
+    // Calculate the year the next Christmas will be
+    let nextChristmasYear = now.getFullYear()
+
+    if (currentMonth == 12 && currentDay > 25) {
+        nextChristmasYear += 1
+    }
+
+    let nextChristmasDate = `Dec 25, ${nextChristmasYear} 00:00:00`,
+        christmasDay = new Date(nextChristmasDate),
+        timeLeft = christmasDay - now
+
+    let days = 0,
+        hours = 0,
+        minutes = 0,
+        seconds = 0
+
+    // Don't calculate the time left if it is Christmas day
+    if (currentMonth != 12 || (currentMonth == 12 && currentDay != 25)) {
+        days = Math.floor(timeLeft / 1000 / 60 / 60 / 24)
+        hours = Math.floor(timeLeft / 1000 / 60 / 60) % 24
+        minutes = Math.floor(timeLeft / 1000 / 60) % 64
+        seconds = Math.floor(timeLeft / 1000) % 64
+    }
+
+    // Show missing days
+    numberData.innerHTML = days < 10 ? `0${days}` : days
+    textData.innerHTML = 'Days'
+
+    // Show missing hours
+    if (currentDay == 24) {
+        numberData.innerHTML = hours < 10 ? `0${hours}` : hours
+        textData.innerHTML = 'Hours'
+    }
+
+    // Show missing minutes. Countdown, 0 hours left, show minutes (00:59)
+    if (currentDay == 24 && hours === 0) {
+        numberData.innerHTML = minutes < 10 ? `0${minutes}` : minutes
+        textData.innerHTML = 'Minutes'
+    }
+
+    // Show missing seconds. Countdown, 0 hours & 0 minutes left, show seconds (00:00:59)
+    if (currentDay == 24 && hours === 0 && minutes === 0) {
+        numberData.innerHTML = seconds < 10 ? `0${seconds}` : seconds
+        textData.innerHTML = 'Seconds'
+    }
+
+    // Show message on Christmas Day
+    if (currentMonth == 12 && currentDay == 25) {
+        titleData.style.display = 'none'
+        msgChristmas.style.display = 'block'
+        msgChristmas.innerHTML = 'Today is Dec 25, Merry Christmas'
+    }
+
+    // Show remaining days & remove Christmas message
+    if (currentMonth == 12 && currentDay == 26) {
+        titleData.style.display = 'block'
+        msgChristmas.style.display = 'none'
+    }
+}
+
+setInterval(christmasCountdown, 1000)
